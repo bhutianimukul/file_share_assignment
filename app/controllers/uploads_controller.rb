1a01e2c8e932b901042c8df9496d998690b6802e
Mukul Bhutiani <mukul.b@browserstack.com>
2024-10-09 02:32:07 +0530

    refactoring

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,9 +1,13 @@
 class UploadsController < ApplicationController
   before_action :authenticate_user, except: [ :show, :download ]
 
+  def new
+    @upload = Upload.new
+  end
+
   def create
     uploaded_file = file_params[:file]
-    file_path, file_name, file_size, content_type = save_file_locally uploaded_file
+    file_path, file_name, file_size, content_type = Upload.save_locally(uploaded_file, logged_in_user.id.to_s)
     @upload = logged_in_user.uploads.new({ name: file_name, file_path: file_path, size: file_size, is_public: file_params[:is_public], content_type: content_type })
     if @upload.save
       redirect_to "/"
@@ -11,10 +15,6 @@ class UploadsController < ApplicationController
       flash[:alert] = "Unable to upload"
       redirect_to "/upload"
     end
-  end
-
-  def new
-    @upload = Upload.new
   end
 
   def index
@@ -46,7 +46,7 @@ class UploadsController < ApplicationController
     if is_logged_in_user
       file_id = permitted_params[:file_id]
       file = logged_in_user.uploads.find_by!(id: file_id)
-      if delete_file file
+      if file.delete_upload
         file.destroy
         redirect_to "/"
       else
@@ -66,7 +66,7 @@ class UploadsController < ApplicationController
       @file = @file_owner.uploads.find_by(id: params[:file_id])
       if @file.present? && File.exist?(@file.file_path) && @file.is_public == true
         @relative_file_path = @file.file_path.sub(Rails.root.join("public").to_s, "")
-        @file_content = read_file_content(@file) if @file.content_type == "text/plain"
+        @file_content = @file.read_text_content if @file.content_type == "text/plain"
         respond_to do |format|
           format.html
           format.json { render json: { file: @file } }
@@ -87,7 +87,7 @@ class UploadsController < ApplicationController
       file_path = @file.file_path
 
       if @file.present? && File.exist?(file_path) && (@file.is_public == true)
-        send_file_securely @file
+        send_upload_securely @file
       else
         flash[:alert] = "File not found"
         render json: { error: "File not found.... Only Public files can be downloaded" }, status: :bad_request
@@ -100,60 +100,16 @@ class UploadsController < ApplicationController
 
   private
 
-  def file_params
-    params.require(:upload).permit(:file, :is_public, :file_id, :user_id, :is_public_updated)
-  end
-
-  def save_file_locally(uploaded_file)
-    save_path = Rails.root.join("public", "uploads", logged_in_user.id.to_s)
-    FileUtils.mkdir_p(save_path) unless File.directory?(save_path)
-    file_name = uploaded_file.original_filename
-    base_name = File.basename(file_name, ".*")
-    extension = File.extname(file_name)
-    timestamp = Time.now.strftime("%Y%m%d%H%M%S")  # Format: YYYYMMDDHHMMSS
-    new_file_name = "#{base_name}_#{timestamp}#{extension}"
-    file_path = File.join(save_path, new_file_name)
-    File.open(file_path, "wb") do |file|
-      file.write(uploaded_file.read)
-    end
-    file_size = convert_file_size uploaded_file.size
-    [ file_path, new_file_name, file_size, uploaded_file.content_type ]
-  end
-
-  def convert_file_size(size_in_bytes)
-    if size_in_bytes < 1024
-      "#{size_in_bytes} bytes"
-    elsif size_in_bytes < 1024 * 1024
-      "#{(size_in_bytes / 1024.0).round(2)} KB"
-    else
-      "#{(size_in_bytes / (1024.0 * 1024.0)).round(2)} MB"
-    end
-  end
-
-  def read_file_content(file)
-    return nil unless file.content_type == "text/plain"
-    File.read(file.file_path)
-  rescue StandardError => e
-    Rails.logger.error "Error reading file: #{e.message}"
-    "Error reading file content"
-  end
-
-  def send_file_securely(file)
-    original_filename = File.basename(file.file_path)
+  def send_upload_securely(upload)
+    original_filename = File.basename(upload.file_path)
     extension = File.extname(original_filename)
     safe_filename = "#{SecureRandom.hex(8)}#{extension}"
-
-    send_data File.read(file.file_path),
+    send_data File.read(upload.file_path), # Not in model because this is a controller method
       filename: safe_filename,
       disposition: "attachment"
   end
 
-  def delete_file(file)
-    if File.exist?(file.file_path)
-      File.delete(file.file_path)
-      true
-    else
-      false
-    end
+  def file_params
+    params.require(:upload).permit(:file, :is_public, :file_id, :user_id, :is_public_updated)
   end
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,47 @@
 class Upload < ApplicationRecord
   belongs_to :user
+
+  def delete_upload
+    puts "Inside delete"
+    if File.exist?(self.file_path)
+      File.delete(self.file_path)
+      true
+    else
+      false
+    end
+  end
+
+  def read_text_content
+    return nil unless self.content_type == "text/plain"
+    File.read(self.file_path)
+  rescue StandardError => e
+    Rails.logger.error "Error reading file: #{e.message}"
+    "Error reading file content"
+  end
+
+  def self.convert_size(size_in_bytes)
+    if size_in_bytes < 1024
+      "#{size_in_bytes} bytes"
+    elsif size_in_bytes < 1024 * 1024
+      "#{(size_in_bytes / 1024.0).round(2)} KB"
+    else
+      "#{(size_in_bytes / (1024.0 * 1024.0)).round(2)} MB"
+    end
+  end
+
+  def self.save_locally(uploaded_file, login_user_id)
+    save_path = Rails.root.join("public", "uploads", login_user_id)
+    FileUtils.mkdir_p(save_path) unless File.directory?(save_path)
+    file_name = uploaded_file.original_filename
+    base_name = File.basename(file_name, ".*")
+    extension = File.extname(file_name)
+    timestamp = Time.now.strftime("%Y%m%d%H%M%S")  # Format: YYYYMMDDHHMMSS
+    new_file_name = "#{base_name}_#{timestamp}#{extension}"
+    file_path = File.join(save_path, new_file_name)
+    File.open(file_path, "wb") do |file|
+      file.write(uploaded_file.read)
+    end
+    file_size = Upload.convert_size uploaded_file.size
+    [ file_path, new_file_name, file_size, uploaded_file.content_type ]
+  end
 end
